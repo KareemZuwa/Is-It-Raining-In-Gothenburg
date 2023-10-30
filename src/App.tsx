87e8f178470a4c02d079921c994d9e5db959ab7c
KarreKult <kareemzuwa@gmail.com>
2023-10-30 14:23:35 +0100

    changing favicon and font settings

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ const App = () => {
   return (
     <>
       <div className="bg-green-600">
-        <header>Header</header>
+        <header className="font-poppins">Header</header>
         <main>Main</main>
       </div>
     </>
