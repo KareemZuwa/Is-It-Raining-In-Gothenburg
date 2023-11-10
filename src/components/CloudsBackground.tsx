1c13aeb4280a880f1096c0aa8ed999c9009f067b
KarreKult <kareemzuwa@gmail.com>
2023-11-10 20:16:49 +0100

    lowering cloud opacity

--- src/components/CloudsBackground.tsx
+++ src/components/CloudsBackground.tsx
@@ -11,22 +11,22 @@ interface CloudsBackgroundProps {
 
 export const CloudsBackground = ({ isDay }: CloudsBackgroundProps) => {
   const { data } = useOneDayForecastQuery();
-  console.log(data?.DailyForecasts[0].Day.Icon)
-  console.log(data?.DailyForecasts[0].Night.Icon)
+  console.log(data?.DailyForecasts[0].Day.Icon);
+  console.log(data?.DailyForecasts[0].Night.Icon);
   const isRain = false;
   const isSnow = false;
 
   return (
     <div className="opacity-30 lg:opacity-100 absolute inset-0 m-auto max-w-screen-2xl">
       <div className="mx-16 flex justify-between overflow-hidden">
-        <div className="pt-[500px] md:pt-[420px]">
+        <div className="pt-[500px] md:pt-[420px] opacity-70">
           <img
             className="animate-fade-down animate-once animate-duration-[5000ms] animate-ease-in-out"
             src="/background/cloud-one.svg"
             alt="cloud-one"
           />
         </div>
-        <div className="relative pt-[420px] md:pt-36">
+        <div className="relative pt-[420px] md:pt-36 opacity-90">
           {!isDay && <Stars />}
           {isDay ? <Sun /> : <Moon />}
           <img
